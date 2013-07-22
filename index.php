3e37b1f6d3910eda968a7bbb48c181471b4b5205
Steve Klise <sk@skli.se>
2013-07-21 23:11:36 -0400

    Add in header setting for lack of a better option

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php get_header();?>
 <?php get_sidebar(); ?>
 
+<?php
+  // UNCOMMENT THE FOLLOWING LINE TO REDIRECT TO THE PROJECTS PAGE.
+ // header('Location: http://itp.nyu.edu/shows/spring2013/category/projects');
+?>
+
+
 <div id="content">
   <div id="image_layer">
   	<img id="postcard-image" src="<?php echo get_option('show_postcard') ?>" alt="ITP Show Postcard Image" />

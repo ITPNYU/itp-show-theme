85356e864474154dcc3917b258d34def832fe7de
Steve Klise <sk@skli.se>
2013-07-21 22:27:00 -0400

    Switch category.php back to the old sql statement setup

--- category.php
+++ category.php
@@ -1,6 +1,53 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 
+<?
+	// This section of PHP appears to be accessing the Projects DB directly
+  $catID = get_query_var('cat');
+  $catName = get_the_category_by_ID($catID); //get name of current category
+
+  $students_catID = get_cat_ID('Students');
+  $instructors_catID = get_cat_ID('Instructors');
+  $classes_catID = get_cat_ID('Related Classes');
+
+  $children_of_cat = get_term_children($catID, 'category');
+
+  if ($catName != 'Projects') {
+
+    // CATEGORY BEING DISPLAYED is one of the following.
+    // Students, Instructors, Related Classes
+    // (NOT Projects, see else)
+
+    $sql = "SELECT tt.term_id, t.name, object_id ";
+    $sql .= "FROM $wpdb->terms t, $wpdb->term_taxonomy tt, $wpdb->term_relationships tr ";
+    $sql .= "WHERE tt.parent = $catID ";
+    $sql .= "AND t.term_id = tt.term_id ";
+    $sql .= "AND tr.term_taxonomy_id = tt.term_taxonomy_id ";
+    $sql .= "ORDER BY t.name";
+    $thePosts = $wpdb->get_results($sql);
+
+    foreach($thePosts as $p) {
+      $tempPost = get_post($p->object_id);
+      $tempPost->filteredItem = $p->name;
+      $filteredPosts[] = $tempPost;
+    }
+
+    $filteredByTitle = false;
+
+  } else {
+    //when category requested is Projects
+    //just request the posts as normal
+    $filteredPosts = get_posts('orderby=post_title&order=ASC&numberposts=500');
+  }
+?>
+
+<?
+  //add css for selected column shading
+  echo "<style>";
+  echo "." . strtolower($catName) . " { font-weight:strong; }";
+  echo "</style>";
+?>
+
 <div id="content">
 	<img id="banner-image" src="<?php echo get_option('show_banner_image') ?>" alt="ITP Show Banner Image" />
 	<h2><?php echo get_option('show_time'); ?></h2>
@@ -24,49 +71,79 @@
 				<a href="<?= bloginfo('url');?>/category/related-classes">Courses</a>
 			</th>
 		</tr>
+<?
+		foreach($filteredPosts as $filteredItem => $post) {
 
-	<?php
-		while (have_posts()) : the_post();
-			$student_category_id = get_cat_ID('Students');
-			$instructors_id = get_cat_id('Instructors');
-			$classes_id = get_cat_id('Related Classes');
-			$categories = get_the_category();
-	?>
-		<tr>
-			<td class="cell title">
-				<a href="<? the_permalink(); ?>">
-				<? the_title(); ?>
-				</a>
-			</td>
-			<td class="cell students">
-				<?
-				foreach($categories as $category) {
-					if($category->parent == $student_category_id) {
-						echo $category->name . "<br>";
-					}
-				}
-				?>
-			</td>
-			<td class="cell instructors">
-				<?
-				foreach($categories as $category) {
-					if($category->parent == $instructors_id) {
-						echo $category->name . "<br>";
-					}
-				}
-				?>
-			</td>
-			<td class="cell related-classes">
-				<?
-				foreach($categories as $category) {
-					if ($category->parent == $classes_id) {
-						echo $category->name . "<br>";
-					}
-				}
-				?>
-			</td>
-		</tr>
-	<? endwhile; ?>
+      setup_postdata($post);
+
+      if (is_project()) {
+
+        $postCategories = wp_get_object_terms($post->ID,'category');
+
+        $students = get_post_parent_category_children('Students', $postCategories); //list the categories that are children of Students
+        $instructors = get_post_parent_category_children('Instructors', $postCategories); //project instructors
+        $classes = get_post_parent_category_children('Related Classes', $postCategories); // project classes
+
+        ?>
+
+          <tr class="row">
+            <!-- title -->
+            <td class="cell title">
+              <a href="<? the_permalink(); ?>">
+              <? the_title(); ?>
+              </a>
+            </td>
+
+            <!-- students column -->
+            <td class="cell students">
+              <?
+              if ($catID == $students_catID) {
+                echo $post->filteredItem;
+              } else {
+                foreach($students as $cat) {
+                  echo $cat->cat_name . "<br>";
+                }
+              }
+              ?>
+            </td>
+
+            <!-- instructors column -->
+            <td class="cell instructors">
+
+              <?
+              if ($catID == $instructors_catID) {
+                echo $post->filteredItem;
+              } else {
+
+              foreach($instructors as $cat) {
+                  echo $cat->cat_name . "<br>";
+                }
+              }
+              ?>
+            </td>
+
+            <!-- classes column -->
+            <td class="cell related-classes">
+              <?
+              if ($catID == $classes_catID) {
+                echo $post->filteredItem;
+              } else {
+
+              foreach($classes as $cat) {
+                  echo $cat->cat_name . "<br>";
+                }
+              }
+              ?>
+            </td>
+          </tr>
+
+
+         <?
+            }
+
+    }
+
+    ?>
 
 	</table>
 

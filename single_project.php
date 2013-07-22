ccd789045ac4341c56b00d10007c220c356a3104
Steve Klise <sk@skli.se>
2013-07-21 22:35:17 -0400

    Switch single_project back to old query

--- single_project.php
+++ single_project.php
@@ -1,80 +1,74 @@
 <div class="post" id="post-<?php the_ID(); ?>">
-	<?
-		$student_category_id = get_cat_ID('Students');
-		$instructors_id = get_cat_ID('Instructors');
-		$classes_id = get_cat_ID('Related Classes');
-		$categories = get_the_category();
-	?>
+  <?
+    $students = get_post_parent_category_children('Students', $postCategories); //list the categories that are children of Students
+    $instructors = get_post_parent_category_children('Instructors', $postCategories); //project instructors
+    $classes = get_post_parent_category_children('Related Classes', $postCategories); // project classes
+  ?>
+  <h2 class="project-title"><?php the_title(); ?></h2>
+  <h3 class="project-author">
+    <?
+      if (isset($students)) {
+        foreach($students as $s) {
+          echo $s->name . "<br>";
+        }
+      }
+    ?>
+  </h3>
 
-	<h2 class="project-title"><?php the_title(); ?></h2>
-	<h3 class="project-author">
-	<?
-		foreach($categories as $category) {
-			if($category->parent == $student_category_id) {
-				echo $category->name . "<br>";
-			}
-		}
-	?>
-	</h3>
+      <h3 class="projectPitch">
+      <?=   stripslashes(getProjectMeta('pitch')) ?>
+      </h3>
 
+      <h3 class="projectURL">
+      <a href="<?= getProjectMeta('externalURL');?> "><?= getProjectMeta('externalURL');?></a>
+      </h3>
 
-	<div class="project-content"><?php the_content(); ?></div>
+      <div>
+        <div id="projectImage">
+          <? if (getProjectMeta('image')) { ?>
+          <img src="<?= getProjectMeta('image'); ?>" />
+          <? } ?>
+        </div>
 
+        <div id="meta_right">
+            <div id="classes">
+              <b>Classes</b>
+              <br>
+              <?
+              foreach($classes as $class) {
+                $theClasses[] = $class->name;
+              }
 
-			<h3 class="projectPitch">
-			<?=   stripslashes(getProjectMeta('pitch')) ?>
-			</h3>
+              echo implode(",",$theClasses);
+              ?>
 
-			<h3 class="projectURL">
-			<a href="<?= getProjectMeta('externalURL');?> "><?= getProjectMeta('externalURL');?></a>
-			</h3>
-
-			<div>
-				<div id="projectImage">
-					<? if (getProjectMeta('image')) { ?>
-					<img src="<?= getProjectMeta('image'); ?>" />
-					<? } ?>
-				</div>
-
-				<div id="meta_right">
-						<div id="classes">
-							<b>Classes</b>
-							<br>
-							<?
-							foreach($classes as $class) {
-								$theClasses[] = $class->name;
-							}
-
-							echo implode(",",$theClasses);
-							?>
-
-						</div>
+            </div>
 
 
-					<br><br>
-						<?
-							$keywords = stripslashes(getProjectMeta('keywords'));
-							if (!empty($keywords)) {
+          <br><br>
+            <?
+              $keywords = stripslashes(getProjectMeta('keywords'));
+              if (!empty($keywords)) {
 
-						?>
-							<div id="keywords">
-								<b>Keywords</b>
-								<br>
-								<?= $keywords; ?>
-							</div>
-						<? } ?>
-				</div>
-			</div>
-		</div>
+            ?>
+              <div id="keywords">
+                <b>Keywords</b>
+                <br>
+                <?= $keywords; ?>
+              </div>
+            <? } ?>
+        </div>
+      </div>
+    </div>
 
 
 
-		<div class="storycontent">
+    <div class="storycontent">
 
-			<?
-			echo stripslashes($post->post_content);
+      <?
+      echo stripslashes($post->post_content);
 
-			       	$extra_meta = array(
+              $extra_meta = array(
                                 "personal-statement" => "Personal Statement",
                                 "background" => "Background",
                                 "audience" =>   "Audience",
@@ -84,15 +78,15 @@
                                 "project-references" => "References"
                               );
 
-			foreach ($extra_meta as $extra  => $emTitle) {
-				$em = getProjectMeta($extra);
-				if ($em) {
-					echo "<br /><br /><b>" . $emTitle . "</b><br />";
-					echo stripslashes($em);
-				}
-			}
+      foreach ($extra_meta as $extra  => $emTitle) {
+        $em = getProjectMeta($extra);
+        if ($em) {
+          echo "<br /><br /><b>" . $emTitle . "</b><br />";
+          echo stripslashes($em);
+        }
+      }
 
-			?>
+      ?>
 
-		</div>
-	</div> <!-- end post -->
+    </div>
+  </div> <!-- end post -->
